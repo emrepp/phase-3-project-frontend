090d141475820c7b3b7dea1de9bb208ac417ce35
Emma Repp <emmarepp.draws@gmail.com>
2023-01-16 22:02:26 -1000

    create new item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
   return (
     <div className="App">
       <h1> Todo App </h1>
+      <div className="input-wrapper">
+        <input type="text" name="todo" placeholder="Create a new todo"
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }} />
+        <button className="add-button">Add</button>
+      </div>
 
     </div>
     
