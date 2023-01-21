2c69b13f737ec38dae3f907e95231006c700fb85
Emma Repp <emmarepp.draws@gmail.com>
2023-01-21 12:34:32 -1000

    ui stuff

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import "./App.css"
 import styled from "styled-components";
 //import { useEffect, useState } from "react";
 
-
 const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
 `;
+const Button = styled.button`
+  display:inline-block;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-radius: 2px;
+  cursor: pointer;
+`;
+const Text = styled.input`
+  border: 2px solid #000;
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div`
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div`
+`;
 function App() {
  return (
-   <Container>
-   </Container>
+  <Container>
+      <div>
+       
+      </div>
+    </Container>
  );
 }
 export default App;
