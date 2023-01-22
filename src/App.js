797743283b88aa70298536a75a13d48a8b185a80
Emma Repp <emmarepp.draws@gmail.com>
2023-01-21 14:14:04 -1000

    working liste without backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import "./App.css"
-import styled from "styled-components";
 import { useState } from "react";
-
+import styled from "styled-components";
+import "./index.css";
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -20,95 +19,93 @@ const Button = styled.button`
 `;
 const Text = styled.input`
   border: 2px solid #000;
+  width: 200px;
+  padding: 5px;
+  border-radius: 2px;
+  margin: 5px;
 `;
 const TaskCount = styled.span`
   margin: 10px;
 `;
 const Tasks = styled.div`
 `;
-const TaskCount = styled.span`
-  margin: 10px;
+const LIST = styled.li`
+    listStyle:"none";
+    text-decoration: "line-through";
 `;
-const Tasks = styled.div`
-`;
-function App() {
+const App = () => {
   const [input, setInput] = useState("");
-const [todoList, setTodoList] = useState([]);
-const [completedTaskCount, setCompletedTaskCount] = useState(0);
-
+  const [completedTaskCount, setCompletedTaskCount] = useState(0);
+  const [todoList, setTodoList] = useState([]);
 const handleClick = () => {
-  const id = todoList.length + 1;
-  setTodoList((prev) => [
-    ...prev,
-    {
-      id: id,
-      task: input,
-      complete: false,
-    },
-  ]);
-  setInput("");
-};
-
-const handleComplete = (id) => {
-  let list = todoList.map((task) => {
-    let item = {};
-    if (task.id == id) {
-      if (!task.complete){
-          //Task is pending, modifying it to complete and increment the count
-          setCompletedTaskCount(completedTaskCount + 1);
-      } 
-      else {
-          //Task is complete, modifying it back to pending, decrement Complete count
-          setCompletedTaskCount(completedTaskCount - 1);
+    const id = todoList.length + 1;
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: input,
+        complete: false,
       }
+    ]);
+    setInput("");
+  };
+  const handleComplete = (id) => {
+    let list = todoList.map((task) => {
+      let item = {};
+      if (task.id == id) {
+        if (!task.complete){
+            //Task is pending, modifying it to complete and increment the count
+            setCompletedTaskCount(completedTaskCount + 1);
+        } 
+        else {
+            //Task is complete, modifying it back to pending, decrement Complete count
+            setCompletedTaskCount(completedTaskCount - 1);
+        }
 item = { ...task, complete: !task.complete };
-    } else item = { ...task };
+      } else item = { ...task };
 return item;
-  });
-  setTodoList(list);
-};
-
-
- return (
-  <Container>
+    });
+    setTodoList(list);
+  };
+return (
+    <Container>
       <div>
-      <h2>Todo List</h2>
+          <h2>Todo List</h2>
           <Text value={input} onInput={(e) =>setInput(e.target.value)} />
           <Button onClick={() => handleClick()}>Add</Button>
         <Tasks>
           <TaskCount>
-            <b>Pending Tasks</b> 
+            <b>Pending Tasks</b> {todoList.length - completedTaskCount}
           </TaskCount>
           <TaskCount>
-            <b>Completed Tasks</b>
+            <b>Completed Tasks</b> {completedTaskCount}
           </TaskCount>
         </Tasks>
         <div>
           <ul>
-          {todoList.map((todo) => {
-    return (
-      <LIST
-        complete={todo.complete}
-        id={todo.id}
-        onClick={() => handleComplete(todo.id)}
-        style={{
-          listStyle: "none",
-          textDecoration: todo.complete && "line-through",
-        }}
-      >
-        {todo.task}
-      </LIST>
-    );
-  })}
+            {todoList.map((todo) => {
+              return (
+                <LIST
+                  complete = {todo.complete}
+                  id={todo.id}
+                  onClick={() => handleComplete(todo.id)}
+                  style={{
+                    listStyle: "none",
+                    textDecoration: todo.complete && "line-through",
+                  }}
+                >
+                  {todo.task}
+                </LIST>
+              );
+            })}
           </ul>
         </div>
-        <Button>Clear</Button>
-       
       </div>
     </Container>
- );
-}
+  );
+};
 export default App;
+
 
 
 
