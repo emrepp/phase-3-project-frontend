0a38330e5daaf948e947d5aba3ad933e11a4d57f
Emma Repp <emmarepp.draws@gmail.com>
2023-01-23 01:20:45 -1000

    more router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './components/Login'
+import Projects from './components/Projects'
+import ProjectDashboard from './components/ProjectDashboard'
+
+import Layout from './containers/Layout'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 
 
-function App {
+function App() {
 
   const [projects, setProjects] = React.useState([])
 
@@ -50,15 +55,80 @@ function App {
         })
       })
   }
-  
-
-
-    return (
-      <div id="app">
-        
-      </div>
+  const handleDeleteProject = (deleteProject) => {
+    const updatedProjects = projects.filter(
+      (project) => project.id !== deleteProject.id
     )
+
+    fetch(`/api/projects/${deleteProject.id}`, {
+      method: 'DELETE',
+    })
+
+    setProjects(updatedProjects)
   }
 
+  const handleUpdatingProject = (changedProject) => {
+    patchProjects(changedProject)
 
-export default App;
+    const updatedProjects = projects.map((project) =>
+      project.id === changedProject.id ? changedProject : project
+    )
+    setProjects(updatedProjects)
+  }
+  const [search, setSearch] = React.useState('')
+  const filterProjects = projects.filter((project) => {
+    return project.title.toLowerCase().includes(search.toLowerCase())
+  })
+}
+
+   
+      return (
+        
+            <Router>
+              <Layout
+                toggleTheme={toggleTheme}
+                mode={mode}
+                projects={projects}
+                search={search}
+                setSearch={setSearch}
+                fetchProjects={fetchProjects}>
+                <Route
+                  exact
+                  path='/'
+                  render={(routerProps) => <Login {...routerProps} />}
+                />
+                <Route
+                  exact
+                  path='/projects'
+                  render={(routerProps) => (
+                    <Projects
+                      {...routerProps}
+                      projects={filterProjects}
+                      mode={mode}
+                      patchProjects={patchProjects}
+                      postProjects={postProjects}
+                      handleUpdatingProject={handleUpdatingProject}
+                      handleDeleteProject={handleDeleteProject}
+                    />
+                  )}
+                />
+                <Route
+                  exact
+                  path='/projects/:id'
+                  render={(routerProps) => (
+                    <ProjectDashboard
+                      {...routerProps}
+                      mode={mode}
+                      handleUpdatingProject={handleUpdatingProject}
+                      handleDeleteProject={handleDeleteProject}
+                    />
+                  )}
+                />
+              </Layout>
+            </Router>
+          
+      )
+      
+    export default App
+    
+  
